c5581d3465aa7641f19a0a70afa659d0a344dc38
John Riebold <jmriebold@gmail.com>
2015-09-24 23:36:20 -0700

    Switched to comparing date only to make it more useful as a daily cron job.

--- equipment/management/commands/sendreminders.py
+++ equipment/management/commands/sendreminders.py
@@ -21,22 +21,22 @@ class Command(BaseCommand):
 
         for reservation in reservations:
             # Check for reservations beginning in less than 24 hours
-            if now + datetime.timedelta(
-                    hours=24) >= reservation.start_date and not reservation.checkout_reminder_sent:
+            if now.date() + datetime.timedelta(
+                    hours=24) >= reservation.start_date.date() and not reservation.checkout_reminder_sent:
                 send_checkout_reminder(reservation)
                 reservation.checkout_reminder_sent = True
                 reservation.save()
 
             # Check for reservations ending in less than 24 hours (that that don't start and end on the same day)
-            elif now + datetime.timedelta(
-                    hours=24) >= reservation.end_date and reservation.start_date.date() != reservation.end_date.date() \
+            elif now.date() + datetime.timedelta(
+                    hours=24) >= reservation.end_date.date() and reservation.start_date.date() != reservation.end_date.date() \
                     and not reservation.return_reminder_sent:
                 send_return_reminder(reservation)
                 reservation.return_reminder_sent = True
                 reservation.save()
 
             # Check for late reservations
-            elif now >= reservation.end_date + datetime.timedelta(
+            elif now.date() >= reservation.end_date.date() + datetime.timedelta(
                     hours=24) and not reservation.returned and not reservation.late_reminder_sent:
                 send_late_reminder(reservation)
                 reservation.late_reminder_sent = True
