38f9a265c4d47533bc8f8c47bdf865524248acfa
John Riebold <jmriebold@gmail.com>
2015-09-30 20:11:13 -0700

    Clean up code.

--- equipment/management/commands/removetempusers.py
+++ equipment/management/commands/removetempusers.py
@@ -1,8 +1,7 @@
 import datetime
-from tzlocal import get_localzone
 
+from tzlocal import get_localzone
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 
 from equipment.models import Status
 
@@ -14,10 +13,11 @@ class Command(BaseCommand):
         now = localtz.localize(datetime.datetime.now())
         now = now.date()
 
+        # Find users with expired permissions
         users = Status.objects.filter(privilege_expiry__lte=now)
 
+        # Reset permissions to default
         for user in users:
-            #user_status = Status.objects.get(user__exact=user)
             user.privilege_level = 4
             user.lab_membership = 'n'
             user.privilege_expiry = None
